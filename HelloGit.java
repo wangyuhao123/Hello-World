f9d85fa41f2ea49f79aef226c10fce062e0e4b6f
yuhao wang <wangyuhao320@163.com>
2017-06-13 14:58:40 +0800

    hello git use java

---
+++ HelloGit.java
@@ -0,0 +1,10 @@
+package github_day01;
+
+/**
+ * Created by wangyuhao on 2017/6/13.
+ */
+public class HelloGit {
+    public static void main(String... args) {
+        System.out.println("hello git");
+    }
+}
